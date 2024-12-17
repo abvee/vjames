90e72e0598f4b70d1229d2405884665b3df9d138
abvee <icaliberdev@gmail.com>
2024-12-18 01:01:45 +0530

    player rectangle and a 2D camera context rectangles for reference

--- src/main.zig
+++ src/main.zig
@@ -7,6 +7,7 @@ const rl = @cImport({
 
 const screen_width = 1440;
 const screen_height = 900;
+const SIDE = 40;
 
 pub fn main() void {
 
@@ -14,13 +15,43 @@ pub fn main() void {
 	rl.InitWindow(screen_width, screen_height, "game");
 	defer rl.CloseWindow();
 
+	// player
+	// world space coords
+	const player: rl.Rectangle = rl.Rectangle{
+		.x = 0 - SIDE / 2,
+		.y = 0 - SIDE / 2,
+		.width = SIDE,
+		.height = SIDE,
+	};
+
+	// camera
+	const camera: rl.Camera2D = rl.Camera2D{
+		.target = rl.Vector2{.x = player.x, .y = player.y},
+		.offset = rl.Vector2{
+			.x = screen_width / 2, .y = screen_height / 2,
+		},
+		.rotation = 0,
+		.zoom = 1,
+	};
+
 	// Main game loop
 	while (!rl.WindowShouldClose()) {
 		rl.BeginDrawing();
 		defer rl.EndDrawing();
 
-		// Do C enums become raylib enums ? No
 		rl.ClearBackground(rl.BLACK);
-		rl.DrawText("Hello world", screen_width / 2, screen_height / 2, 20, rl.LIGHTGRAY);
+
+		// camera
+		rl.BeginMode2D(camera);
+		defer rl.EndMode2D();
+		rl.DrawRectangleRec(player, rl.RED);
+
+		// context
+		const context: [2]rl.Rectangle = [_]rl.Rectangle{
+			rl.Rectangle{.x = SIDE, .y = SIDE, .width = SIDE, .height = SIDE},
+			rl.Rectangle{.x = 2 * SIDE, .y = 2 * SIDE, .width = SIDE, .height = SIDE},
+		};
+		rl.DrawRectangleRec(context[0], rl.RAYWHITE);
+		rl.DrawRectangleRec(context[1], rl.RAYWHITE);
 	}
 }
