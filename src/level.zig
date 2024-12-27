8ffdc814edcf14be420fb2ed6826ea96f821b129
abvee <icaliberdev@gmail.com>
2024-12-27 19:58:27 +0530

    lvl1 number corrected delim_atoi and patoi and full file reading

--- src/level.zig
+++ src/level.zig
@@ -15,8 +15,24 @@ pub fn load(allocator: std.mem.Allocator, path: []const u8) !void {
 	const reader = file.reader();
 
 	// Number of rectangles in the level
-	const number = atoi(try reader.readUntilDelimiterAlloc(allocator, '\n', MAX));
-	std.debug.print("{d}\n", .{number + 1});
+	const number = patoi(try reader.readUntilDelimiterAlloc(allocator, '\n', MAX));
+
+	var i: u8 = 0;
+	while (i < number):(i += 1) {
+		const line = try reader.readUntilDelimiterAlloc(allocator, '\n', MAX);
+
+		var p: [*]const u8 = @ptrCast(line);
+		var j: u8 = 0; // iterator
+		for (line) |l| {
+			if (l == ',') {
+				std.debug.print("{d} ", .{delim_atoi(p[0..j], ',') + 1});
+				p = @ptrCast(&line[j + 1]);
+			}
+			else
+				j += 1;
+		}
+		std.debug.print("\n", .{});
+	}
 }
 test "level loading test" {
 	// Allocator
@@ -29,11 +45,22 @@ test "level loading test" {
 }
 
 // Basic Atoi function for positive integers
-fn atoi(s: []const u8) u32 {
+fn patoi(s: []const u8) u32 {
 	var ret: u32 = 0;
 	for (s) |i| {
 		ret *= 10;
 		ret += i - '0';
 	}
 	return ret;
+}
+
+// atoi until a delimiter
+inline fn delim_atoi(s: []const u8, delim: u8) f32 {
+	var r: f32 = 0;
+	for (s) |i| {
+		if (i == delim)
+			return r;
+		r = (r * 10) + @as(f32, @floatFromInt(i - '0'));
+	}
+	return r;
 }
