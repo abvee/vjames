4dcaa695069f79022d6d78c3207fe764c048c73f
abvee <icaliberdev@gmail.com>
2024-12-29 13:09:20 +0530

    Delimter atoi removed, replaced with General purpose positive atoi read rectangle values from the file and convert them to floats

--- src/level.zig
+++ src/level.zig
@@ -15,26 +15,37 @@ pub fn load(allocator: std.mem.Allocator, path: []const u8) !void {
 	const reader = file.reader();
 
 	// Number of rectangles in the level
-	const number = patoi(try reader.readUntilDelimiterAlloc(allocator, '\n', MAX));
+	const number = patoi(u32, try reader.readUntilDelimiterAlloc(allocator, '\n', MAX));
 
 	var i: u8 = 0;
 	while (i < number):(i += 1) {
 		const line = try reader.readUntilDelimiterAlloc(allocator, '\n', MAX);
+		var j: u8 = 0; // line index
+		var k: u8 = 0; // index of previous segment
 
-		var p: [*]const u8 = @ptrCast(line);
-		var j: u8 = 0; // iterator
-		for (line) |l| {
-			if (l == ',') {
-				std.debug.print("{d} ", .{delim_atoi(p[0..j], ',') + 1});
-				p = @ptrCast(&line[j + 1]);
-			}
-			else
-				j += 1;
-		}
-		std.debug.print("\n", .{});
+		while (line[j] != ',') : (j += 1) {}
+		std.debug.print("{d} ", .{patoi(f32, line[k..j]) + 1});
+		j += 1;
+		k = j;
+
+		while (line[j] != ',') : (j += 1) {}
+		j += 1;
+		k = j;
+		std.debug.print("{d} ", .{patoi(f32, line[k..j]) + 1});
+
+		while (line[j] != ',') : (j += 1) {}
+		std.debug.print("{d} ", .{patoi(f32, line[k..j]) + 1});
+		j += 1;
+		k = j;
+
+		while (j < line.len and line[j] != ',') : (j += 1) {}
+		std.debug.print("{d} ", .{patoi(f32, line[k..j]) + 1});
+		j += 1;
+		k = j;
 	}
 }
 test "level loading test" {
+	std.debug.print("--LEVEL LOADING--\n", .{});
 	// Allocator
 	var arena = std.heap.ArenaAllocator.init(
 		std.heap.page_allocator
@@ -45,22 +56,19 @@ test "level loading test" {
 }
 
 // Basic Atoi function for positive integers
-fn patoi(s: []const u8) u32 {
-	var ret: u32 = 0;
+fn patoi(T: type, s: []const u8) T {
+	var ret: T = 0;
 	for (s) |i| {
 		ret *= 10;
-		ret += i - '0';
+
+		if (T == f32)
+			ret += @as(T, @floatFromInt(i - '0'))
+		else
+			ret += i - '0';
 	}
 	return ret;
 }
-
-// atoi until a delimiter
-inline fn delim_atoi(s: []const u8, delim: u8) f32 {
-	var r: f32 = 0;
-	for (s) |i| {
-		if (i == delim)
-			return r;
-		r = (r * 10) + @as(f32, @floatFromInt(i - '0'));
-	}
-	return r;
+test "patoi" {
+	std.debug.print("--PATOI--\n", .{});
+	std.debug.print("{d}\n", .{patoi(f32, "40") + 0.1});
 }
