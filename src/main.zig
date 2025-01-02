050456883ad4e2f50154cd9d5821d60632351cfa
abvee <icaliberdev@gmail.com>
2025-01-02 10:06:04 +0530

    Draw references over everything in world space Mouse position tracking

--- src/main.zig
+++ src/main.zig
@@ -89,25 +89,31 @@ pub fn main() !void {
 		defer rl.EndDrawing();
 
 		rl.ClearBackground(rl.BLACK);
-		draw_references();
 
 		// camera
-		rl.BeginMode2D(camera);
-		defer rl.EndMode2D();
-		rl.DrawRectangleRec(player.box, rl.RED);
+		{
+			rl.BeginMode2D(camera);
+			defer rl.EndMode2D();
+			rl.DrawRectangleRec(player.box, rl.RED);
 
-		// draw level
-		for (lvl) |l| {
-			rl.DrawRectangleRec(l, rl.RAYWHITE);
+			// draw level
+			for (lvl) |l| {
+				rl.DrawRectangleRec(l, rl.RAYWHITE);
+			}
 		}
+		draw_references();
+
 	}
 }
 
 // draw all references in screen space
 inline fn draw_references() void {
-		const center = rl.Vector2{.x = screen_width / 2, .y = screen_height / 2};
+	const center = rl.Vector2{.x = screen_width / 2, .y = screen_height / 2};
 
-		// gun circles
-		rl.DrawCircleLinesV(center, 1.41 * 20, rl.SKYBLUE);
-		rl.DrawCircleLinesV(center, 1.41 * 20 + 10, rl.PURPLE);
+	// gun circles
+	rl.DrawCircleLinesV(center, 1.41 * 20, rl.SKYBLUE);
+	rl.DrawCircleLinesV(center, 1.41 * 20 + 10, rl.PURPLE);
+
+	// center to mouse position
+	rl.DrawLineV(center, rl.GetMousePosition(), rl.GREEN);
 }
