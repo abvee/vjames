b7f68adac9082ac57731aeeecb3cd6f27ef458a6
abvee <icaliberdev@gmail.com>
2025-02-08 20:39:02 +0530

    Command line port option

--- server/main.zig
+++ server/main.zig
@@ -2,12 +2,25 @@ const std = @import("std");
 const net = std.net;
 const posix = std.posix;
 
+// stdout
+const stdout = std.io.getStdOut().writer();
+
 var addr = net.Address.initIp4(
 	[4]u8{0,0,0,0}, // accept connections from any address
 	12271, // default port
 );
 
+const ParameterError = error {IncorrectArguments};
 pub fn main() !void {
+
+	// get port from cmdline
+	if (get_port()) |port| addr.setPort(port)
+	else |err| switch (err) {
+		ParameterError.IncorrectArguments => {},
+		else => return err,
+	}
+	try stdout.print("Using port: {}\n", .{addr.getPort()});
+
 	const sock = try posix.socket(
 		posix.AF.INET,
 		posix.SOCK.DGRAM,
@@ -29,4 +42,37 @@ pub fn main() !void {
 
 	// sendto
 	_ = try posix.sendto(sock, "bye bye world", 0, &client.any, @sizeOf(net.Address));
+}
+
+// get port from the command line and return it
+inline fn get_port() !u16 {
+	switch (std.os.argv.len) {
+		1 => {
+			try stdout.print("No arguments found, using default port: {}\n", .{addr.getPort()});
+			return addr.getPort();
+		},
+		3 => {
+			if (!std.mem.orderZ(u8, std.os.argv[1], "-p").compare(.eq)) {
+				try stdout.print("Unkown argument, using default port: {}\n", .{addr.getPort()});
+				return ParameterError.IncorrectArguments;
+			}
+			if (std.fmt.parseInt(
+					u16, std.os.argv[2][0..std.mem.len(std.os.argv[2])],10
+				)
+			) |port| return port
+			else |err| return err;
+		},
+		else => {
+			std.debug.print(
+				"Incorrect number of commandline argument found, using default port: {}\n",
+				.{addr.getPort()},
+			);
+			return ParameterError.IncorrectArguments;
+		},
+	}
+}
+test "get_port" {
+	if (get_port()) |port|
+		std.debug.print("{}\n", .{port})
+	else |err| return err;
 }
