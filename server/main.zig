da605018fa0169b4c1dd7cc39181ceeafa18a768
abvee <icaliberdev@gmail.com>
2025-02-21 13:56:27 +0530

    broadcast thread and functions

--- server/main.zig
+++ server/main.zig
@@ -4,25 +4,26 @@ const posix = std.posix;
 const assert = std.debug.assert;
 const stdout = std.io.getStdOut().writer();
 
-var addr = net.Address.initIp4(
-	[4]u8{0,0,0,0}, // accept connections from any address
-	12271, // default port
-);
 
-const MAX_PLAYERS = 10;
-
+// Types
 // generic packet structure
 const packetData = [12]u8;
 const packet = [1 + @sizeOf(packetData)]u8;
 // a packet is 1 byte for (id) and the rest of the packet
 
+// globals
+var addr = net.Address.initIp4(
+	[4]u8{0,0,0,0}, // accept connections from any address
+	12271, // default port
+);
+const MAX_PLAYERS = 10;
 var conns: [MAX_PLAYERS]?net.Address = .{null} ** MAX_PLAYERS;
 // support a maximum of 10 connections
 var positions: [MAX_PLAYERS]packetData =
 	[_][@sizeOf(packetData)]u8{[_]u8{0} ** @sizeOf(packetData)} ** MAX_PLAYERS;
 // [10][8]u8{0 filled};
-
 var no_conns: u8 = 0; // current number of connections
+var sockp: *const posix.socket_t = undefined;
 
 const ParameterError = error{IncorrectArguments};
 pub fn main() !void {
@@ -41,16 +42,25 @@ pub fn main() !void {
 		posix.SOCK.DGRAM,
 		posix.IPPROTO.UDP,
 	);
+	sockp = &sock;
 	defer posix.close(sock);
 
 	// bind
 	try posix.bind(sock, &addr.any, addr.getOsSockLen());
 
+	// start broadcasting
+	const broadcast_thread = try std.Thread.spawn(
+		.{},
+		broadcast_handler,
+		.{},
+	);
+	defer broadcast_thread.join();
+
 	var buf: packet = .{0} ** @sizeOf(packet);
 	// packet buffer
 	// refer to client networking for packet structure.
 
-	// wait for packets
+	// Wait for new packets
 	while (true) {
 		var client: net.Address = undefined;
 		var client_len: posix.socklen_t = @sizeOf(net.Address);
@@ -105,7 +115,7 @@ inline fn update_position(data: packet, client: net.Address) PossibleCheaters!vo
 const LobbyErrors = error{ServerFull};
 // Add to the conns array
 inline fn add_conn(client: net.Address) LobbyErrors!u8 {
-	if (no_conns > 10)
+	if (no_conns > MAX_PLAYERS)
 		return LobbyErrors.ServerFull;
 
 	for (conns, 0..conns.len) |con, i| {
@@ -120,6 +130,35 @@ inline fn add_conn(client: net.Address) LobbyErrors!u8 {
 
 	// this should never be reached
 	return LobbyErrors.ServerFull;
+}
+
+// broadcast id's position to all other players
+fn broadcast(id: u8) !void {
+	const pack: packet = [1]u8{id} ++ positions[id];
+	for (conns, 0..conns.len) |conn, i| {
+		if (i == id or conn == null)
+			continue; // skip our player and non existant player
+		_ = try posix.sendto(
+			sockp.*,
+			&pack,
+			0,
+			&conn.?.any,
+			conn.?.getOsSockLen(),
+		);
+		// TODO: if sending fails, then we shouldn't just return from the
+		// program.
+	}
+}
+
+fn broadcast_handler() !void {
+	while (true) {
+		// TODO: probably needs like lerp or something with whatever timing we
+		// choose. We do this on the client, don't forget to do it.
+		std.time.sleep(std.time.ns_per_s * 0.5);
+		for (0..MAX_PLAYERS) |i|
+			if (conns[i]) |_| try broadcast(@intCast(i));
+		// TODO: don't shit yourself if a single packet fails to send
+	}
 }
 
 // get port from the command line and return it
